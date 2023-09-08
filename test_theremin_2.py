70f21de96463c7fad97c81b7d8e1f300f682e4f6
Rob Cranfill <robcranfill@gmail.com>
2023-09-07 20:52:04 -0700

    Uses VL53L0X!

--- test_theremin_2.py
+++ test_theremin_2.py
@@ -9,20 +9,23 @@ References:
     https://paia.com/thereton/
 
 """
-
+# standard libs
 import audiobusio
 import audiocore
 import audiomixer
 import board
+import busio
 import math
 import os
 import synthio
 import time
 
+# 3rd party libs
 import ulab.numpy as np
+import adafruit_vl53l0x
 
 
-# interferes with the audio?
+# auto-reload interferes with the audio?
 import supervisor
 supervisor.runtime.autoreload = False  # CirPy 8 and above
 print(f"supervisor.runtime.autoreload = {supervisor.runtime.autoreload}")
@@ -33,28 +36,50 @@ print(f"supervisor.runtime.autoreload = {supervisor.runtime.autoreload}")
 board_type = os.uname().machine
 if "Pico" in board_type:
     print("Detected Pico!")
+    board_name = "pico"         # my own var
     PIN_BIT_CLOCK   = board.GP16
     PIN_WORD_SELECT = board.GP17
     PIN_DATA        = board.GP18
+    # as per https://learn.adafruit.com/raspberry-pi-pico-led-arcade-button-midi-controller-fighter/wiring-stemma-for-pico
+    PIN_STEMMA_BLUE = board.GP0
+    PIN_STEMMA_YELLOW = board.GP1
+
 elif "Feather" in board_type:
     print("Detected Feather!")
+    board_name = "feather"
     PIN_BIT_CLOCK   = board.D9
     PIN_WORD_SELECT = board.D10
     PIN_DATA        = board.D11
+
 else:
     print("Unsupported board?")
     raise Exception
+
+# set up i2c
+if board_name == "pico":
+    try:
+        i2c = busio.I2C(board.GP1, board.GP0, frequency=1000000)
+        print("i2c OK!")
+        sensor = adafruit_vl53l0x.VL53L0X(i2c)
+        sensor.measurement_timing_budget = 20000
+        print("sensor OK!")
+    except:
+        print("i2c init failed!")
+else:
+    i2c = None # TODO: FIXME:
 
 # We need a pretty big buffer to stop I/O noise! Why?
 # If we use 2 channels, 32K is insufficient on Feather! 48K is mostly ok, 64K almost entirely :-/
 MIXER_BUFFER_SIZE = 32*1024 
 
-SYNTH_SAMPLE_RATE = 22050
+SYNTH_SAMPLE_RATE  = 22050
 WAVE_SAMPLE_SIZE   =  1024
 WAVE_SAMPLE_VOLUME = 32767
 
 sine_wave = np.array(
     np.sin(np.linspace(0, 2*np.pi, WAVE_SAMPLE_SIZE, endpoint=False)) * WAVE_SAMPLE_VOLUME, dtype=np.int16)
+
+# ------- sigmoid stuff - worth it?
 
 # FIXME: ramp num could be rather low, for a coarse LFO, right? why not?
 RAMP_SAMPLE_SIZE = 100
@@ -85,6 +110,8 @@ sigmoid_ramp_5  = np.array(sigmoid_ramp_5_f,  dtype=np.int16)
 # for x in sigmoid_ramp_5:
 #     print(x)
 
+# ------- end sigmoid stuff
+
 
 audio = audiobusio.I2SOut(PIN_BIT_CLOCK, PIN_WORD_SELECT, PIN_DATA)
 
@@ -99,6 +126,27 @@ _mixer.voice[0].play(_synth)
 
 
 # ---------------- end setup
+
+def freq_from_dist(d):
+    return d * 10
+
+def test_3(synth, tof_sensor):
+
+    # We create just one Note, and bend it for all our "song notes".
+    #
+    f1 = synthio.midi_to_hz(50) # FIXME
+    lfo = synthio.LFO(waveform=linear_ramp, once=True, scale=0) # scale=0: no bend to start with
+    n = synthio.Note(f1, waveform=sine_wave, bend=lfo)
+    synth.press(n)
+
+    while True:
+        dist = tof_sensor.range
+        if dist > 100 and dist < 1000:
+            f2 = freq_from_dist(dist)
+            slide_from_f1_to_f2(n, f1, f2, seconds=0.1)
+            print(f" range {dist} mm -> {f1} Hz")
+            f1 = f2
+            # time.sleep(.01)
 
 
 def test_2(synth):
@@ -166,7 +214,6 @@ def test_1(synth):
             time.sleep(1)
 
 
-
 def slide_from_f1_to_f2(note: synthio.Note, start_freq, target_freq, seconds=1.0):
     """ Create the proper LFO to take us from current to target freq, and trigger it. 
     
@@ -179,16 +226,25 @@ def slide_from_f1_to_f2(note: synthio.Note, start_freq, target_freq, seconds=1.0
     # In order to bend to the target freq, 
     # the lfo 'scale' is the base-2 log of the ratio of the frequencies.
     lfo.scale = math.log(target_freq/start_freq, 2)
-    print(f"  slide: {start_freq:.0f} -> {target_freq:.0f} (lfo.scale = {lfo.scale:.2f})")
+    # print(f"  slide: {start_freq:.0f} -> {target_freq:.0f} (lfo.scale = {lfo.scale:.2f})")
 
     # reset the note's freq to the starting freq
     note.frequency = start_freq
-    lfo.rate = 1/seconds
 
+    # trigger the LFO, which will take us from the start freq to the target freq
+    # in {seconds}
+    #
+    lfo.rate = 1/seconds
     lfo.retrigger()
 
 
-test_2(_synth)
+# main code:
+#
+# we run a test
+#
+test_3(_synth, sensor)
+# test_2(_synth)
+# test_1(_synth)
 
 print("test_theremin_2.py done!")
 while True:
